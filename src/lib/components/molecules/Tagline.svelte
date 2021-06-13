7eee0afb64f8e723edeba40bfa6e669cd34a1e8f
Jesse Vermeulen <jesse.vermeulen3@hva.nl>
2021-06-13 10:56:08 +0200

    Tagline component

---
+++ src/lib/components/molecules/Tagline.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Icon from '$lib/components/atoms/Icon.svelte';
+</script>
+
+<h1 class="grid gap-3 text-center text-8xl">
+	<div class="font-clash">
+		Reaching people<span class="font-sentient text-xl text-left inline-block ml-4"
+			>by <br /> building</span
+		>
+	</div>
+
+	<span class="font-basteleur block">impactful experiences</span>
+
+	<div class="font-clash inline-block">
+		<span class="font-sentient text-xl text-right inline-block mr-4">for <br /> the</span>modern www
+		<div class="icon inline-block align-middle -mx-5">
+			<Icon gradient icon="globe-simple" />
+		</div>
+		rld
+	</div>
+</h1>
+
+<style>
+	h1 .font-sentient {
+		line-height: 98%;
+	}
+
+	.icon {
+		width: 5.25rem;
+	}
+</style>
