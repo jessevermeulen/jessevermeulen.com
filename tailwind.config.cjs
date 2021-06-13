26e8badb0ab547efb1e2ab52d5941e6b1a35c682
Jesse Vermeulen <jesse.vermeulen3@hva.nl>
2021-06-13 10:54:53 +0200

    Import typography

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,14 @@
 module.exports = {
-	mode: "jit",
-	purge: [
-		"./src/**/*.{html,js,svelte,ts}",
-	],
+	mode: 'jit',
+	purge: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {},
+		fontFamily: {
+			body: ['var(--font-stack-base)'],
+			clash: ['var(--font-stack-clash)'],
+			basteleur: ['var(--font-stack-basteleur)'],
+			sentient: ['var(--font-stack-sentient)']
+		}
 	},
-	plugins: [],
+	plugins: []
 };
