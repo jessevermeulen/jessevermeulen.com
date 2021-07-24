479fbc0915748ccf989aa256948fda71151db7b8
Jesse Vermeulen <jesse.vermeulen3@hva.nl>
2021-07-24 10:31:33 +0200

    Create Hero component

---
+++ src/lib/components/atoms/Hero.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { onMount } from 'svelte';
+	import * as animateScroll from 'svelte-scrollto';
+
+	let innerHeight;
+	let height;
+
+	onMount(() => {
+		height = innerHeight;
+	});
+</script>
+
+<svelte:window bind:innerHeight />
+
+<section class="container h-screen" style="max-height: {height}px;">
+	<div class="grid gap-8 pt-16">
+		<h1>
+			<span class="after:block">Digital Product Designer</span>Amsterdam — NL
+		</h1>
+		<p class="text-2xl">
+			Looking for a 20-week-long Visual Design internship, starting this September.
+		</p>
+		<button
+			type="button"
+			on:click={() => animateScroll.scrollTo({ element: '#about' })}
+			class="text-2xl animate-bounce-simple w-[fit-content]">↓</button
+		>
+	</div>
+</section>
