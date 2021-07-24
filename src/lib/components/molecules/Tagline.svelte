badf50ad55946f740c56ba64c13b90c4c1390149
Jesse Vermeulen <jesse.vermeulen3@hva.nl>
2021-07-24 10:35:03 +0200

    Edit Tagline component

--- src/lib/components/molecules/Tagline.svelte
+++ src/lib/components/molecules/Tagline.svelte
@@ -2,30 +2,37 @@
 	import Icon from '$lib/components/atoms/Icon.svelte';
 </script>
 
-<h1 class="grid gap-3 text-center text-8xl">
+<h1 class="grid text-center">
 	<div class="font-clash">
-		Reaching people<span class="font-sentient text-xl text-left inline-block ml-4"
+		Reaching people<span class="inline-block ml-[1%] font-sentient text-left"
 			>by <br /> building</span
 		>
 	</div>
 
-	<span class="font-basteleur block">impactful experiences</span>
+	<span class="block font-basteleur">impactful experiences</span>
 
-	<div class="font-clash inline-block">
-		<span class="font-sentient text-xl text-right inline-block mr-4">for <br /> the</span>modern www
-		<div class="icon inline-block align-middle -mx-5">
-			<Icon gradient icon="globe-simple" />
-		</div>
+	<div class="inline-block font-clash">
+		<span class="inline-block mr-[1%] font-sentient text-right">for <br /> the</span>modern www
+		<span class="inline-block w-[7%] m-[-2%] align-middle">
+			<Icon custom icon="globe-simple" />
+			<!-- <Icon icon="globe-simple" /> -->
+		</span>
 		rld
 	</div>
 </h1>
 
 <style>
-	h1 .font-sentient {
-		line-height: 98%;
-	}
+	h1 {
+		font-size: 7vw;
+		line-height: 100%;
 
-	.icon {
-		width: 5.25rem;
+		@media (min-aspect-ratio: 17/9) {
+			/* font-size: 6vw; */
+		}
+
+		.font-sentient {
+			line-height: 98%;
+			font-size: 20%;
+		}
 	}
 </style>
