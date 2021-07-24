510c4fdb4a5045fd142870a42152c0291785c3b3
Jesse Vermeulen <jesse.vermeulen3@hva.nl>
2021-07-24 10:30:37 +0200

    Add animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,29 @@ module.exports = {
 	mode: 'jit',
 	purge: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {},
 		fontFamily: {
 			body: ['var(--font-stack-base)'],
 			clash: ['var(--font-stack-clash)'],
 			basteleur: ['var(--font-stack-basteleur)'],
 			sentient: ['var(--font-stack-sentient)']
+		},
+		extend: {
+			screens: {
+				portrait: { raw: '(orientation: portrait)' }
+			},
+			keyframes: {
+				bounceSimple: {
+					'0%, 100%': {
+						transform: 'translate3d(0, 25%, 0)'
+					},
+					'50%': {
+						transform: 'translate3d(0, 0, 0)'
+					}
+				}
+			},
+			animation: {
+				'bounce-simple': 'bounceSimple 2s cubic-bezier( 0.445, 0.05, 0.55, 0.95) infinite'
+			}
 		}
 	},
 	corePlugins: {
