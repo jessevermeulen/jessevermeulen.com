4451a674372424426bc52a7fd31556cb93cf65f5
Jesse Vermeulen <jesse.vermeulen3@hva.nl>
2021-07-24 10:32:07 +0200

    Create Logotype component

---
+++ src/lib/components/atoms/Logotype.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Icon from '$lib/components/atoms/Icon.svelte';
+</script>
+
+<div class="grid gap-3 mx-auto max-w-max text-sm h-[fit-content]">
+	<div class="grid grid-flow-col justify-between items-center font-clash font-medium">
+		20
+		<span class="w-7 text-gray-900">
+			<Icon custom icon="jv-logomark" />
+		</span>
+		21
+	</div>
+	<p class="font-clash font-medium uppercase">Amsterdam</p>
+</div>
+
+<style>
+	div {
+		letter-spacing: 0.2rem;
+	}
+</style>
