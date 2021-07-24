bc08b6946eeb951d3ce4d445dedd82825f3de464
Jesse Vermeulen <jesse.vermeulen3@hva.nl>
2021-07-24 10:32:16 +0200

    Create Intro component

---
+++ src/lib/components/molecules/Intro.svelte
@@ -0,0 +1,25 @@
+<section id="about" class="container mx-auto grid grid-cols-6 sm:grid-cols-8 lg:grid-cols-12 gap-4">
+	<div class="grid col-span-6 gap-8 text-xl pt-24 pb-32">
+		<p>
+			Jesse Vermeulen is an Amsterdam based Digital Product Designer with over 8 years of experience
+			in both design and development. He worked as a frontend developer at agencies as PXL.Widgets
+			and FBI Design and developed his skillset by working on a variety of commercial and personal
+			projects.
+		</p>
+		<p>
+			Throughout the years he broadened his focus by exploring all the facets of UI, UX and
+			typography design, all while keeping up with the latest web development trends.
+		</p>
+		<p>
+			As of 2021, he co-founded the company Stypt, a software-as-a-service startup that aims to help
+			businesses manage their customer appointments efficiently using an appointment tool designed
+			for ease of use.
+		</p>
+		<p>
+			For more information say hello at
+			<a href="mailto:hello@jessevermeulen.com" class="before:block border-b border-gray-900"
+				>hello@jessevermeulen.com</a
+			>
+		</p>
+	</div>
+</section>
