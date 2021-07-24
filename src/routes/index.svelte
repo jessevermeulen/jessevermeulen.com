61bfe6b0616c5196c387130015f9b7d7e75d9f01
Jesse Vermeulen <jesse.vermeulen3@hva.nl>
2021-07-24 10:34:36 +0200

    Extend index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,9 @@
 <script>
-	import Hero from '$lib/components/organisms/Hero.svelte';
+	import Hero from '$lib/components/atoms/Hero.svelte';
+	import Intro from '$lib/components/molecules/Intro.svelte';
+	import Work from '$lib/components/organisms/Work.svelte';
 </script>
 
 <Hero />
+<Intro />
+<Work />
