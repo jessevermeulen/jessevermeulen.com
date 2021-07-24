add1d619bacec2f8db8b828597954fa37d9fc439
Jesse Vermeulen <jesse.vermeulen3@hva.nl>
2021-07-24 10:33:38 +0200

    Set svelte-scrollto global options

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,11 @@
 <script>
 	import '../styles/index.css';
 	import Header from '$lib/components/atoms/Header.svelte';
+	import * as animateScroll from 'svelte-scrollto';
+
+	animateScroll.setGlobalOptions({
+		duration: 200
+	});
 </script>
 
 <Header />
