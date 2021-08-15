d38d7f51234bf23ebc1ad6d8ad335eb13764f0f4
Jesse Vermeulen <jesse.vermeulen3@hva.nl>
2021-08-15 21:19:49 +0200

    Improve styling

--- src/lib/content/layout.svelte
+++ src/lib/content/layout.svelte
@@ -7,14 +7,16 @@
 
 <article id={slug}>
 	<div class="container mx-auto grid grid-cols-6 sm:grid-cols-8 lg:grid-cols-12 gap-4 pb-8">
-		<h1 class="grid col-start-5 col-span-full w-max pt-48 pb-40 text-2xl sm:text-4xl xl:text-6xl">
+		<h1
+			class="grid lg:col-start-5 col-span-full w-max pt-24 pb-20 md:pt-32 md:pb-28 text-4xl sm:text-6xl"
+		>
 			{title}
 		</h1>
-		<p class="date grid col-span-4">{@html date}</p>
+		<p class="grid col-span-4 self-start">{@html date}</p>
 		<p class="grid col-span-7">{description}</p>
 	</div>
 
-	<div class="px-2 grid sm:grid-cols-2 gap-2">
+	<div class="px-2 grid gap-2">
 		<slot />
 	</div>
 </article>
