1ee206b761485389fd8babf7a1dbb23ad6295002
Jesse Vermeulen <jesse.vermeulen3@hva.nl>
2021-08-15 21:10:33 +0200

    Configure svimg

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,18 @@
 import { mdsvex } from 'mdsvex';
 import { mdsvexConfig } from './mdsvex.config.js';
 import netlify from '@sveltejs/adapter-netlify';
+import imagePreprocessor from 'svimg';
 import preprocess from 'svelte-preprocess';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
 	preprocess: [
 		mdsvex(mdsvexConfig),
+		imagePreprocessor({
+			inputDir: 'static',
+			outputDir: 'static'
+		}),
 		preprocess({
 			defaults: {
 				style: 'postcss'
