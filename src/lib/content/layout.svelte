fcc6864d8fbe4bfbb756e41f706609984490e31f
Jesse Vermeulen <jesse.vermeulen3@hva.nl>
2021-08-15 17:04:59 +0200

    Add mdsvex layout

---
+++ src/lib/content/layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let title;
+	export let description;
+	export let date;
+	export let slug;
+</script>
+
+<article id={slug}>
+	<div class="container mx-auto grid grid-cols-6 sm:grid-cols-8 lg:grid-cols-12 gap-4 pb-8">
+		<h1 class="grid col-start-5 col-span-full w-max pt-48 pb-40 text-2xl sm:text-4xl xl:text-6xl">
+			{title}
+		</h1>
+		<p class="date grid col-span-4">{@html date}</p>
+		<p class="grid col-span-7">{description}</p>
+	</div>
+
+	<div class="px-2 grid sm:grid-cols-2 gap-2">
+		<slot />
+	</div>
+</article>
