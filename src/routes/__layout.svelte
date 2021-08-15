93757d6e9fa98af0bb958dbe0005d888c9899b97
Jesse Vermeulen <jesse.vermeulen3@hva.nl>
2021-08-15 21:18:10 +0200

    Add global styling

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,14 +1,9 @@
 <script>
-	import * as animateScroll from 'svelte-scrollto';
-
 	import '$lib/styles/index.css';
+
+	import Meta from '$lib/components/atoms/Meta.svelte';
 	import Header from '$lib/components/atoms/Header.svelte';
 	import Footer from '$lib/components/atoms/Footer.svelte';
-	import Meta from '$lib/components/atoms/Meta.svelte';
-
-	animateScroll.setGlobalOptions({
-		duration: 200
-	});
 </script>
 
 <Meta />
