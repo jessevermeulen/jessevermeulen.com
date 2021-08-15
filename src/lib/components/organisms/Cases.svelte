a4dc7a4b0e98db1a26e050537f3a7c2049d49e0e
Jesse Vermeulen <jesse.vermeulen3@hva.nl>
2021-08-15 21:13:39 +0200

    Add Cases component

---
+++ src/lib/components/organisms/Cases.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { onMount } from 'svelte';
+	import lozad from 'lozad';
+
+	import Stypt from '$lib/content/stypt.svelte.md';
+	import SeparatedUnited from '$lib/content/separated-united.svelte.md';
+	import Oscillate from '$lib/content/oscillate.svelte.md';
+	import SadIndie from '$lib/content/sad-indie.svelte.md';
+
+	onMount(() => {
+		const observer = lozad();
+		observer.observe();
+	});
+
+	const components = [Stypt, SeparatedUnited, Oscillate, SadIndie];
+</script>
+
+<section id="cases" class="pt-24 pb-8">
+	<div class="container mx-auto">
+		<h2>Selected Work</h2>
+	</div>
+
+	{#each components as component}
+		<svelte:component this={component} />
+	{/each}
+</section>
