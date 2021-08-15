0f7e6b4b39e7abb411c2b05595f404ebe59a4081
Jesse Vermeulen <jesse.vermeulen3@hva.nl>
2021-08-15 21:17:01 +0200

    Retrieve mdsvex files using a get function

---
+++ src/routes/index.json.js
@@ -0,0 +1,33 @@
+import pMap from 'p-map';
+import { basename } from 'path';
+
+export async function get() {
+	// Import all .svelte.md files in the directory
+	const modules = import.meta.glob('../lib/content/*.svelte.md');
+
+	// Run a map over each module
+
+	// Check out the docs for p-map if this looks confusing, it's  basically
+	// Array.map(...) but for promises
+
+	const cases = await pMap(Object.entries(modules), async function ([filename, module]) {
+		// Import the component. The metadata here is added by MDSveX and mirrors
+		// the front matter.
+
+		const { metadata } = await module();
+
+		return {
+			title: metadata.title,
+			date: metadata.date,
+			description: metadata.description,
+			slug: basename(filename, '.svelte.md') // Generate a slug we can link to
+		};
+	});
+
+	// Sort cases by descending date
+	// cases.sort((a, b) => (a.date > b.date ? -1 : 1));
+
+	return {
+		body: { cases }
+	};
+}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,35 @@
+<script context="module">
+	/**
+	 * @type {import('@sveltejs/kit').Load}
+	 */
+	export async function load({ fetch }) {
+		// Use a `limit` querystring parameter to fetch a limited number of posts
+		// e.g. fetch('posts.json?limit=5') for 5 most recent posts
+		const res = await fetch('/index.json');
+
+		if (res.ok) {
+			return {
+				props: {
+					cases: await res.json()
+				}
+			};
+		}
+
+		return {
+			status: res.status,
+			error: new Error(`Could not load /index.json`)
+		};
+	}
+</script>
+
 <script>
 	import Hero from '$lib/components/atoms/Hero.svelte';
 	import Intro from '$lib/components/molecules/Intro.svelte';
-	import Work from '$lib/components/organisms/Work.svelte';
+	import Cases from '$lib/components/organisms/Cases.svelte';
+
+	export let cases;
 </script>
 
 <Hero />
-<!-- <Intro /> -->
-<!-- <Work /> -->
+<Intro />
+<Cases {...cases} />
